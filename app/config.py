28aab998b83d6e7606b475b60258e41d5123f85c
D14m0nDS <antoanargirov1404@gmail.com>
2025-05-01 19:26:03 +0300

    Fixing database connection on cloud #4

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'filesystem'
     JWT_COOKIE_SECURE = True
